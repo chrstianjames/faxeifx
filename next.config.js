be234044690495e9caf8ff277200011b9591e28c
chrstianjames <113699127+chrstianjames@users.noreply.github.com>
2024-02-02 06:04:16 +0800

    Initialize mainlive-preview

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/chatgpt-ai-template/chat',
+        permanent: true,
+      },
+    ];
+  },
+  assetPrefix: '/chatgpt-ai-template',
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'i.ibb.co',
+      'scontent.fotp8-1.fna.fbcdn.net',
+    ],
+    // Make ENV
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
